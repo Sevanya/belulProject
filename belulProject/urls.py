7a56923612551efd261524767ef79c88d2206680
Sevanya <vsevolod.krupenko@yandex.ru>
2017-08-21 14:29:47 +0300

    static collect

--- belulProject/urls.py
+++ belulProject/urls.py
@@ -17,8 +17,8 @@ Including another URLconf
 from django.conf.urls import url, include
 from django.contrib import admin
 admin.autodiscover()
-from django.conf import settings
-from django.conf.urls.static import static
+# from django.conf import settings
+# from django.conf.urls.static import static
 
 from django.contrib.flatpages.views import *
 from . import views
@@ -29,19 +29,20 @@ admin.site.site_header = settings.ADMIN_SITE_HEADER
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
-    # url(r'^$', views.mainView, name='mainPage'),
-    # url(r'^', include('connect.urls')),
-    # url(r'^ckeditor/', include('ckeditor_uploader.urls')),
-    # url(r'^company/about/', flatpage, {'url':'/company/about/'}, name='about'),
-    # url(r'^company/goods/', flatpage, {'url':'/company/goods/'}, name='goods'),
-    # url(r'^company/park/', flatpage, {'url':'/company/park/'}, name='avtopark'),
-    # url(r'^stations/adresses/', flatpage, {'url':'/stations/adresses/'}, name='adresa'),
-    # url(r'^stations/services/', flatpage, {'url':'/stations/services/'}, name='uslugi'),
-    # url(r'^sale/conditionterms/', flatpage, {'url':'/sale/conditionterms/'}, name='postavka'),
-    # url(r'^sale/shipping/', flatpage, {'url':'/sale/shipping/'}, name='dostavka'),
-    # url(r'^contacts/', flatpage, {'url':'/contacts/'}, name='contacts'),
-    # url(r'^', include('article.urls')),
-    # url(r'^', include('price.urls')),
-] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT) + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT) 
+    url(r'^$', views.mainView, name='mainPage'),
+    url(r'^', include('connect.urls')),
+    url(r'^ckeditor/', include('ckeditor_uploader.urls')),
+    url(r'^company/about/', flatpage, {'url':'/company/about/'}, name='about'),
+    url(r'^company/goods/', flatpage, {'url':'/company/goods/'}, name='goods'),
+    url(r'^company/park/', flatpage, {'url':'/company/park/'}, name='avtopark'),
+    url(r'^stations/adresses/', flatpage, {'url':'/stations/adresses/'}, name='adresa'),
+    url(r'^stations/services/', flatpage, {'url':'/stations/services/'}, name='uslugi'),
+    url(r'^sale/conditionterms/', flatpage, {'url':'/sale/conditionterms/'}, name='postavka'),
+    url(r'^sale/shipping/', flatpage, {'url':'/sale/shipping/'}, name='dostavka'),
+    url(r'^contacts/', flatpage, {'url':'/contacts/'}, name='contacts'),
+    url(r'^', include('article.urls')),
+    url(r'^', include('price.urls')),
+] 
+# + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT) + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT) 
 
 # /venv/lib/python2.7/site-packages/django/contrib/admin/static/admin
