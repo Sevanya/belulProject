d3feca439d6575d890837bca01c593560edb5a5e
Sevanya <vsevolod.krupenko@yandex.ru>
2017-08-21 14:20:27 +0300

    static collect

--- belulProject/settings.py
+++ belulProject/settings.py
@@ -38,7 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'article',
-    'django.contrib.sites',
+    # 'django.contrib.sites',
     'django.contrib.flatpages',
     'connect',
     'ckeditor',
