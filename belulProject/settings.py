ba510f50ea66cb910459e77571c00879092d73ad
Sevanya <vsevolod.krupenko@yandex.ru>
2017-08-21 14:21:41 +0300

    static collect

--- belulProject/settings.py
+++ belulProject/settings.py
@@ -39,7 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'article',
     'django.contrib.sites',
-    # 'django.contrib.flatpages',
+    'django.contrib.flatpages',
     'connect',
     'ckeditor',
     'ckeditor_uploader',
@@ -55,7 +55,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    # 'django.contrib.flatpages.middleware.FlatpageFallbackMiddleware',
+    'django.contrib.flatpages.middleware.FlatpageFallbackMiddleware',
 
 ]
 
